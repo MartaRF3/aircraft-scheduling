dc50c3e29439f2b08cb4464dc5710213c7dff443
JRM_MucHyp <jana.ruiz.morell@muenchenerhyp.com>
2021-08-06 20:23:46 +0200

    Sort functionality

--- src/components/Flights.tsx
+++ src/components/Flights.tsx
@@ -4,6 +4,9 @@ import { useGetFlightsQuery } from "../hooks/useGetFlightsQuery";
 import { LoadingSpinner } from "./LoadingSpinner";
 import { ErrorMessage } from "./ErrorMessage";
 import { FlightCard } from "./FlightCard";
+import { useAppContext } from "../context/AppContext";
+import { SortOption } from "../types/AppContextTypes";
+import { Flight } from "../types/FlightTypes";
 
 type Props = {};
 
@@ -15,6 +18,65 @@ export const Flights: React.FC<Props> = () => {
     isError,
     error,
   } = useGetFlightsQuery();
+
+  const [appContext] = useAppContext();
+  const { sort } = appContext;
+
+  const sortFlights = (): Flight[] => {
+    let orderedList: Flight[] = [];
+
+    switch (sort) {
+      case SortOption.DepartureASC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          a.readable_departure.localeCompare(b.readable_departure)
+        );
+        return orderedList;
+      }
+      case SortOption.ArrivalASC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          a.readable_arrival.localeCompare(b.readable_arrival)
+        );
+        return orderedList;
+      }
+      case SortOption.DestinationASC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          a.destination.localeCompare(b.destination)
+        );
+        return orderedList;
+      }
+      case SortOption.OriginASC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          a.origin.localeCompare(b.origin)
+        );
+        return orderedList;
+      }
+      case SortOption.DepartureDESC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          b.readable_departure.localeCompare(a.readable_departure)
+        );
+        return orderedList;
+      }
+      case SortOption.ArrivalDESC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          b.readable_arrival.localeCompare(a.readable_arrival)
+        );
+        return orderedList;
+      }
+      case SortOption.DestinationDESC: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          b.destination.localeCompare(a.destination)
+        );
+        return orderedList;
+      }
+
+      default: {
+        orderedList = flights.data.sort((a: Flight, b: Flight) =>
+          b.origin.localeCompare(a.origin)
+        );
+        return orderedList;
+      }
+    }
+  };
 
   if (isLoading) return <LoadingSpinner />;
 
@@ -30,7 +92,7 @@ export const Flights: React.FC<Props> = () => {
           rows="small"
           columns={{ count: "fill", size: ["small"] }}
         >
-          {flights?.data.map((flight) => (
+          {sortFlights().map((flight) => (
             <FlightCard key={flight.id} flight={flight} />
           ))}
         </Grid>
